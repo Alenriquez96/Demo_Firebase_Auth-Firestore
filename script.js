1bab7875ec924c3bbb55139b393d17c8e7a50c38
Alenriquez96 <alberto.enriquez96@gmail.com>
2022-07-14 16:59:25 +0200

    add/firebase_demo

--- script.js
+++ script.js
@@ -1,11 +1,77 @@
 // Your web app's Firebase configuration
-let firebaseConfig = {
-// Your object here
+const firebaseConfig = {
+  apiKey: "AIzaSyC8gk8R9laPud5MwM1A24P8N75zZwBOuwU",
+  authDomain: "auth-prueba-85c5c.firebaseapp.com",
+  projectId: "auth-prueba-85c5c",
+  storageBucket: "auth-prueba-85c5c.appspot.com",
+  messagingSenderId: "168571272867",
+  appId: "1:168571272867:web:83de718b410293135c0916"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();// db representa mi BBDD
+
+//PASOS PARA QUE SALGA EL LOG CON GOOGLE
+let provider = new firebase.auth.GoogleAuthProvider();
+
+//Función de login con Auth
+async function login (){
+  try{
+      const response = await firebase.auth().signInWithPopup(provider);
+      console.log(response);
+
+      let newUser = {
+        email: response.user.email,
+        name: response.user.displayName,
+      }
+      
+      db.collection("users")
+        .where("email", "==", response.user.email)
+        .get()
+        .then((querySnapshot) => {
+          console.log(querySnapshot);
+          if(querySnapshot.size == 0){
+            db.collection("users")
+            .add(newUser)
+            .then((docRef) => console.log("Document written with ID: ", docRef.id))
+            .catch((error) => console.error("Error adding document: ", error));
+          } else{
+            console.log(`usuario de nombre ${response.user.displayName} ya existe en la BBDD firestore.`);
+          }
+        });
+
+      return response.user;
+
+  }catch(error){  
+      console.log(error);
+  }
+}
+
+//DESLOGUEARSE AUTH
+const signOutGoogle = async () => {
+  try {
+      let user = await firebase.auth().currentUser;
+      await firebase.auth().signOut();
+      console.log("Sale del sistema: "+user.email);
+      localStorage.clear();
+  } catch (error) {
+      console.log("hubo un error: "+error);
+  }
+}
+document.getElementById("logout").addEventListener("click", signOutGoogle);
+
+const buttonLogin = document.getElementById("google");
+
+buttonLogin.addEventListener("click", async (e)=>{
+    try{
+        await login();
+    } catch(error) {}
+});
+
+
+
+
 
 const createUser = (user) => {
   db.collection("users")
@@ -131,12 +197,15 @@ document.getElementById("form2").addEventListener("submit",function(event){
 })
 document.getElementById("salir").addEventListener("click", signOut);
 
-// Listener de usuario en el ssitema
-// Controlar usuario logado
-firebase.auth().onAuthStateChanged(function(user) {
+document.getElementById("loggedUsers").addEventListener("click", function(){
+  // Listener de usuario en el ssitema
+  // Controlar usuario logado
+  firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       console.log(`Está en el sistema:${user.email} ${user.uid}`);
     } else {
       console.log("no hay usuarios en el sistema");
     }
   });
+})
+
